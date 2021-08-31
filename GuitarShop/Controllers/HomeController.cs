f904bdfc4fadee6d170d9e8d449c57f723ab72a7
Kenny-star <54686876+Kenny-star@users.noreply.github.com>
2021-08-30 21:34:20 -0400

    Add project files.

---
+++ GuitarShop/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GuitarShop.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            //Navigate View Model 
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            //Navigate View Model 
+            return View();
+        }
+    }
+}
---
+++ GuitarShop/Controllers/ProductController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using GuitarShop.Models;
+
+namespace GuitarShop.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Detail(string id)
+        {
+            //Get every productId from the Database to send as a Response view.
+            Product product = DB.GetProduct(id);
+            return View(product);
+        }
+
+        public IActionResult List()
+        {
+            //Get every products from the Database to send as a Response view.
+            List<Product> products = DB.GetProducts();
+            return View(products);
+        }
+    }
+}
